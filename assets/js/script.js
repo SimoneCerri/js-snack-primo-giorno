567499f45ade54dbd05669da3bd74d0644020171
SimoneCerri <iosonosimone@hotmail.it>
2024-02-16 15:02:04 +0100

    finish the exercises, only one commit cuz i forgot to push step by step at the end of every exercise

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+//Exercise 1
+let number = 100;
+console.log(number);
+
+//Exercise 2
+let PI = 3.14;
+console.log(PI);
+
+//Exercise 2/B
+PI = 3.1416
+console.log(PI) //I was expecting to see and error but nothing wrong apparently
+
+//Exercise 2/C
+let radius = 8;
+let circle = (radius * PI * 2);
+console.log(circle);
+
+//Exercise 3
+let name = ("Carlo");
+name = ("Marco");
+console.log(name); //as expected the name is changed, but js don't like the variable "name" ?
+
+
